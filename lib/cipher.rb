9dd930e4a6431f1bea24e530a5eacd8840a970bb
DV <dvislearning@gmail.com>
2016-05-24 14:17:28 -0700

    refactor code

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -7,25 +7,46 @@ shift_input = gets.chomp.to_i
 =end
 
 
+# def caesar_cipher(text, shift)
+# 	modelo_shift = shift%26 
+# 	modelo_shift = 26 if modelo_shift == 0 #Necessary for when shift is a multiple of 26
+# 	0.upto(text.length-1) do |letter|
+# 		if text[letter] =~ (/[A-Za-z]/)
+# 			if ((65..90) === (text[letter].ord+modelo_shift)) || ((97..122) === (text[letter].ord+modelo_shift) && text[letter] =~ (/[a-z]/))
+# 				text[letter] = (text[letter].ord + modelo_shift)
+# 			elsif  text[letter] =~ (/[A-Z]/)
+# 				text[letter] = ((((text[letter].ord) + modelo_shift)-91) + 65).chr
+# 			elsif  text[letter] =~ (/[a-z]/)
+# 				text[letter] = ((((text[letter].ord) + modelo_shift)-123) + 97).chr
+# 			end
+
+# 	end
+# end
+# text
+# end
+
+# caesar_cipher("hello", 15)
+
 def caesar_cipher(text, shift)
-	modelo_shift = shift%26 
+	modelo_shift = shift%26
 	modelo_shift = 26 if modelo_shift == 0 #Necessary for when shift is a multiple of 26
-	0.upto(text.length-1) do |letter|
-		if text[letter] =~ (/[A-Za-z]/)
-			if ((65..90) === (text[letter].ord+modelo_shift)) || ((97..122) === (text[letter].ord+modelo_shift) && text[letter] =~ (/[a-z]/))
-				text[letter] = (text[letter].ord + modelo_shift)
-			elsif  text[letter] =~ (/[A-Z]/)
-				text[letter] = ((((text[letter].ord) + modelo_shift)-91) + 65).chr
-			elsif  text[letter] =~ (/[a-z]/)
-				text[letter] = ((((text[letter].ord) + modelo_shift)-123) + 97).chr
-			end
+	message = text.split("")
+	message.map! do |character|
+		if character =~ (/[A-Za-z]/)
+		# 	if ((65..90) === (text[letter].ord+modelo_shift)) || ((97..122) === (text[letter].ord+modelo_shift) && text[letter] =~ (/[a-z]/))
+		# 		text[letter] = (text[letter].ord + modelo_shift)
+		# 	elsif  text[letter] =~ (/[A-Z]/)
+		# 		text[letter] = ((((text[letter].ord) + modelo_shift)-91) + 65).chr
+		# 	elsif  text[letter] =~ (/[a-z]/)
+		# 		text[letter] = ((((text[letter].ord) + modelo_shift)-123) + 97).chr
+		# 	end
 
 	end
 end
-text
+message.join(" ")
 end
 
-caesar_cipher("hello", 15)
+caesar_cipher("hello world", 0)
 
 #ABCDEFGHIJKLMNOPQRSTUVWXYZ
 #abcdefghijklmnopqrstuvwxyz
