875883d307c4a736352ddf5a1bc9b96f2949fb24
DV <dvislearning@gmail.com>
2016-05-25 09:37:54 -0700

    Complete writing tests

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -16,7 +16,26 @@ describe 'cipher' do
 
 	 it "returns correct string when shifted 25 positions" do
 	 	expect(caesar_cipher("hello", 25)).to eq("gdkkn")
-	 end	
+	 end
 
+	 it "properly escapes whitespae" do
+ 		expect(caesar_cipher(" he  llo   world   ", 2)).to eq(" jg  nnq   yqtnf   ")
+	 end
+	 
+	 it "properly escapes punctuation" do
+ 		expect(caesar_cipher("he$$llo wo.r.ld?", 2)).to eq("jg$$nnq yq.t.nf?")
+	 end
+
+	 it "returns original string when shifted a very large multiple of 26" do
+ 		expect(caesar_cipher("hello world xyz", 24804)).to eq("hello world xyz")
+	 end
+
+	 it "returns correct string when shifted a very large non-multiple of 26" do
+ 		expect(caesar_cipher("hello world xyz", 24817)).to eq("uryyb jbeyq klm")
+	 end
+	 
+	 it "returns correct string when shifted a a negative value" do
+ 		expect(caesar_cipher("hello world xyz abc", -2)).to eq("fcjjm umpjb vwx yza")
+	 end	 
 
 end
